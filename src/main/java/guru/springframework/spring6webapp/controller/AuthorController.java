a427742b6f41973770a7ecb86920f0432ca67557
Konrad Bartczak <kbartczak@sii.pl>
2023-08-27 15:24:17 +0200

    two view

---
+++ src/main/java/guru/springframework/spring6webapp/controller/AuthorController.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6webapp.controller;
+
+import guru.springframework.spring6webapp.service.AuthorService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+    String test;
+        List
+        model.addAttribute("authors", authorService.findAll());
+        return "books";
+    }
+}
